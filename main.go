07b0ddbed34e1206b17f7120730cd486f7e7cc7c
jtagcat <git-12dbd862@jtag.cat>
2022-02-07 21:58:25 +0200

    exludes-includes = nightmare

--- main.go
+++ main.go
@@ -115,6 +115,11 @@ func routine(mc mainCtx) error {
 	}
 
 	errChan := make(chan error)
+	go func() {
+		for e := range errChan {
+			log.Print(e)
+		}
+	}()
 	// not launching goroutines for rate-limiting sanity
 	for _, playlist := range config.Playlists {
 		processPlaylist(mc, errChan, playlist)
@@ -123,33 +128,41 @@ func routine(mc mainCtx) error {
 		processProfile(mc, errChan, profile)
 	}
 
-	go func() {
-		for e := range errChan {
-			log.Print(e)
-		}
-	}()
-
 	return mc.r.Commit(&git.Signature{Name: committerName, Email: "", When: time.Now()}, "routine run")
 }
 
 type exportedPlaylist struct { // spotify.FullPlaylist with modifications
-	SimplePlaylist spotify.SimplePlaylist
-	Description    string
-	Followers      spotify.Followers
-	Tracks         []spotify.PlaylistTrack
+	spotify.SimplePlaylist `yaml:"meta,omitempty"`
+	Description            string `yaml:"description,omitempty"`
+	spotify.Followers      `yaml:"followers,omitempty"`
+	Tracks                 []minPlaylistTrack `yaml:"tracks,omitempty"`
+}
+type minPlaylistTrack struct {
+	AddedAt string   `yaml:"added_at,omitempty"`
+	AddedBy string   `yaml:"added_by,omitempty"` // other values empty(?)
+	IsLocal bool     `yaml:"is_local,omitempty"`
+	Track   minTrack `yaml:"track,omitempty"`
+}
+type minTrack struct {
+	ID           spotify.ID             `yaml:"id,omitempty"`
+	Name         string                 `yaml:"name,omitempty"`
+	Artists      []spotify.SimpleArtist `yaml:"artists,omitempty"`
+	ExternalURLs map[string]string      `yaml:"external_urls,omitempty"`
 }
 
 func processPlaylist(mc mainCtx, errChan chan<- error, id spotify.ID) {
-	pl, err := mc.c.GetPlaylist(mc.ctx, id, spotify.Fields("tracks(!items),owner(!followers)"))
+	pl, err := mc.c.GetPlaylist(mc.ctx, id, spotify.Fields("id,name,collaborative,images,owner(id,display_name),public,snapshot_id,description,followers"))
 	if err != nil {
 		errChan <- fmt.Errorf("couldn't get playlist %q: %w", id, err)
 		return
 	}
 
-	plt, err := mc.c.GetPlaylistTracks(mc.ctx, id, spotify.Fields("items(addedby(!followers),track(!availablemarkets,album(!availablemarkets))"))
+	plt, err := mc.c.GetPlaylistTracks(mc.ctx, id, // display_name is not included in the response
+		spotify.Fields("next,items(added_at,added_by(id,display_name),is_local,track(!album))")) // can't exclude more than 1 item? !available_markets
 	if err != nil {
 		errChan <- fmt.Errorf("couldn't get playlist tracks for %q: %w", id, err)
 	}
+
 	for page := 1; ; page++ {
 		err = mc.c.NextPage(mc.ctx, plt)
 		if err == spotify.ErrNoMorePages {
@@ -160,7 +173,13 @@ func processPlaylist(mc mainCtx, errChan chan<- error, id spotify.ID) {
 		}
 	}
 
-	e, err := yaml.Marshal(exportedPlaylist{SimplePlaylist: pl.SimplePlaylist, Description: pl.Description, Tracks: plt.Tracks})
+	var pltMin []minPlaylistTrack
+	for _, t := range plt.Tracks {
+		pltMin = append(pltMin, minPlaylistTrack{AddedAt: t.AddedAt, AddedBy: t.AddedBy.ID, IsLocal: t.IsLocal,
+			Track: minTrack{ID: t.Track.ID, Name: t.Track.Name, Artists: t.Track.Artists, ExternalURLs: t.Track.ExternalURLs}})
+	}
+
+	e, err := yaml.Marshal(&exportedPlaylist{SimplePlaylist: pl.SimplePlaylist, Description: pl.Description, Followers: pl.Followers, Tracks: pltMin})
 	if err != nil {
 		errChan <- fmt.Errorf("couldn't marshal playlist %q: %w", id, err)
 		return
@@ -172,14 +191,42 @@ func processPlaylist(mc mainCtx, errChan chan<- error, id spotify.ID) {
 	}
 }
 
+type profileWithPlaylists struct {
+	*spotify.User `yaml:"profile,omitempty"`
+	Playlists     []minProfilePlaylist `yaml:"playlists,omitempty"`
+}
+
+type minProfilePlaylist struct { // spotify.SimplePlaylist with modifications
+	ID   spotify.ID `json:"id"`
+	Name string     `json:"name"`
+}
+
 func processProfile(mc mainCtx, errChan chan<- error, id spotify.ID) {
-	pl, err := mc.c.GetUsersPublicProfile(mc.ctx, id)
+	pf, err := mc.c.GetUsersPublicProfile(mc.ctx, id)
 	if err != nil {
 		errChan <- fmt.Errorf("couldn't get profile %q: %w", id, err)
 		return
 	}
 
-	e, err := yaml.Marshal(&pl)
+	pft, err := mc.c.GetPlaylistsForUser(mc.ctx, string(id)) // can't use fields
+	if err != nil {
+		errChan <- fmt.Errorf("couldn't get profile playlists for %q: %w", id, err)
+	}
+	for page := 1; ; page++ {
+		err = mc.c.NextPage(mc.ctx, pft)
+		if err == spotify.ErrNoMorePages {
+			break
+		}
+		if err != nil {
+			errChan <- fmt.Errorf("couldn't get profile playlists for %q: page %q %w", id, page, err)
+		}
+	}
+	var mpl []minProfilePlaylist
+	for _, pl := range pft.Playlists {
+		mpl = append(mpl, minProfilePlaylist{pl.ID, pl.Name})
+	}
+
+	e, err := yaml.Marshal(&profileWithPlaylists{User: pf, Playlists: mpl})
 	if err != nil {
 		errChan <- fmt.Errorf("couldn't marshal profile %q: %w", id, err)
 		return
